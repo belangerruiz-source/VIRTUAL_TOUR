7e787554c8f53749d896c4c99d0abb37c49a09a9
belangerruiz-source <belangerruiz@gmail.com>
2026-03-17 11:11:58 -0600

    Add files via upload

---
+++ channels.js
@@ -0,0 +1,20 @@
+const API_KEY="AIzaSyDsCEmTUrXyr7E8RJQqbZt4AV0IN9XQHiI"
+
+const CHANNELS=[
+
+{
+name:"CIUDADES",
+playlist:"PLCKML06wO4bnsJRlpO_YycBsJaqE_RfqL"
+},
+
+{
+name:"HISTORIA",
+playlist:"PLCKML06wO4blGfodaIFpQirOfS1Vna6rL"
+},
+
+{
+name:"UNIVERSO",
+playlist:"PLCKML06wO4blWKoFOvTcBLI0P5GxTRG45"
+}
+
+]
