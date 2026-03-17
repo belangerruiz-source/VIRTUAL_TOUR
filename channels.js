ee26c7c5b0ee77e0741cec73423f9be87aea48d4
belangerruiz-source <belangerruiz@gmail.com>
2026-03-17 14:40:12 -0600

    Add files via upload

--- channels.js
+++ channels.js
@@ -1,41 +1,14 @@
 const canales = [
 {
   nombre: "CIUDADES",
-  videos: [
-    "VIDEO_ID_1",
-    "VIDEO_ID_2",
-    "VIDEO_ID_3"
-  ],
-  duraciones: [
-    600,
-    720,
-    540
-  ]
+  playlist: "PLCKML06wO4bnsJRlpO_YycBsJaqE_RfqL"
 },
 {
   nombre: "HISTORIA",
-  videos: [
-    "VIDEO_ID_4",
-    "VIDEO_ID_5",
-    "VIDEO_ID_6"
-  ],
-  duraciones: [
-    800,
-    900,
-    700
-  ]
+  playlist: "PLCKML06wO4blGfodaIFpQirOfS1Vna6rL"
 },
 {
   nombre: "UNIVERSO",
-  videos: [
-    "VIDEO_ID_7",
-    "VIDEO_ID_8",
-    "VIDEO_ID_9"
-  ],
-  duraciones: [
-    1000,
-    850,
-    920
-  ]
+  playlist: "PLCKML06wO4blWKoFOvTcBLI0P5GxTRG45"
 }
 ];
--- tv.js
+++ tv.js
@@ -1,4 +1,48 @@
 let player;
+let cache = {};
+
+const API_KEY = "AIzaSyDsCEmTUrXyr7E8RJQqbZt4AV0IN9XQHiI";
+
+async function cargarCanales() {
+
+  for (let i = 0; i < canales.length; i++) {
+    const canal = canales[i];
+
+    const videos = await obtenerVideos(canal.playlist);
+    const duraciones = await obtenerDuraciones(videos);
+
+    cache[i] = {
+      videos,
+      duraciones
+    };
+  }
+
+  iniciarCanal();
+}
+
+async function obtenerVideos(playlistId) {
+  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlistId}&key=${API_KEY}`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return data.items.map(i => i.snippet.resourceId.videoId);
+}
+
+async function obtenerDuraciones(ids) {
+  const url = `https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=${ids.join(",")}&key=${API_KEY}`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return data.items.map(v => convertirISO(v.contentDetails.duration));
+}
+
+function convertirISO(iso) {
+  const match = iso.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+  const h = parseInt(match[1]) || 0;
+  const m = parseInt(match[2]) || 0;
+  const s = parseInt(match[3]) || 0;
+  return h * 3600 + m * 60 + s;
+}
 
 function calcularPosicion(duraciones) {
   const ahora = Math.floor(Date.now() / 1000);
@@ -16,14 +60,16 @@ function calcularPosicion(duraciones) {
 
 function iniciarCanal() {
   const ch = new URLSearchParams(window.location.search).get("ch") || 0;
+
+  const data = cache[ch];
   const canal = canales[ch];
 
   document.getElementById("canal").innerText = canal.nombre;
 
-  const pos = calcularPosicion(canal.duraciones);
+  const pos = calcularPosicion(data.duraciones);
 
   player = new YT.Player("player", {
-    videoId: canal.videos[pos.index],
+    videoId: data.videos[pos.index],
     playerVars: {
       autoplay: 1,
       controls: 0,
@@ -32,8 +78,8 @@ function iniciarCanal() {
     events: {
       onStateChange: function(e) {
         if (e.data === 0) {
-          pos.index = (pos.index + 1) % canal.videos.length;
-          player.loadVideoById(canal.videos[pos.index]);
+          pos.index = (pos.index + 1) % data.videos.length;
+          player.loadVideoById(data.videos[pos.index]);
         }
       }
     }
@@ -41,5 +87,5 @@ function iniciarCanal() {
 }
 
 function onYouTubeIframeAPIReady() {
-  iniciarCanal();
+  cargarCanales();
 }
